926452451193206373410d68f1e6e739a935f922
unknown <wnsrl981228@gmail.com>
2022-10-17 13:16:28 +0900

    branch 테스트

---
+++ 1Week_Mission/src/main/java/com/ll/finalproject/app/member/entity/Member.java
@@ -0,0 +1,25 @@
+package com.ll.finalproject.app.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ll.finalproject.app.base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String email;
+    private String nickname;
+}
