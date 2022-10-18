e08d6a964e96d5db865ac7982d036b944192f775
unknown <wnsrl981228@gmail.com>
2022-10-18 10:44:59 +0900

    feat : 로그인 기초 기능 구현
    MemberContext와 MemberSecurityService 추가
    로그인 폼 간단하게 구현

---
+++ 1Week_Mission/src/main/java/com/ll/finalproject/app/security/dto/MemberContext.java
@@ -0,0 +1,21 @@
+package com.ll.finalproject.app.security.dto;
+
+import com.ll.finalproject.app.member.entity.Member;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+@Getter
+public class MemberContext extends User {
+    private final Long id;
+    private final String username;
+
+
+    public MemberContext(Member member, List<GrantedAuthority> authorities) {
+        super(member.getUsername(), member.getPassword(), authorities);
+        this.id = member.getId();
+        this.username = member.getUsername();
+    }
+}
