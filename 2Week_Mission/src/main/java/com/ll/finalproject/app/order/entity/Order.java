8bc7bb6ce9ba6550bd64e47a97129577b6660e99
unknown <wnsrl981228@gmail.com>
2022-10-25 11:01:38 +0900

    feat : 토스페이먼츠 연동

--- 2Week_Mission/src/main/java/com/ll/finalproject/app/order/entity/Order.java
+++ 2Week_Mission/src/main/java/com/ll/finalproject/app/order/entity/Order.java
@@ -64,4 +64,13 @@ public class Order extends BaseEntity {
 
         return payPrice;
     }
+    public String getName() {
+        String name = orderItems.get(0).getProduct().getSubject();
+
+        if ( orderItems.size() > 1 ) {
+            name += " 외 %d권".formatted(orderItems.size() - 1);
+        }
+
+        return name;
+    }
 }
