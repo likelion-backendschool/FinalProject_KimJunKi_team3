bea37376f6c79e24cd15621eb47873097fbf46db
unknown <wnsrl981228@gmail.com>
2022-11-01 13:14:48 +0900

    feat : 입력된 연월에 해당하는 정산데이터 생성을 위한 주문품목의 결제날짜 범위 구하기

--- 3Week_Mission/src/main/java/com/ll/finalproject/app/rebate/controller/AdmRebateController.java
+++ 3Week_Mission/src/main/java/com/ll/finalproject/app/rebate/controller/AdmRebateController.java
@@ -1,9 +1,14 @@
 package com.ll.finalproject.app.rebate.controller;
 
+import com.ll.finalproject.util.Ut;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/adm/rebate")
@@ -13,6 +18,20 @@ public class AdmRebateController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public String showMakeData() {
         return "adm/rebate/makeData";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseBody
+    @PostMapping("/makeData")
+    public String makeData(String yearMonth) {
+        int monthEndDay = Ut.date.getEndDayOf(yearMonth);
+
+        String fromDateStr = yearMonth + "-01 00:00:00.000000";
+        String toDateStr = yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay);
+        LocalDateTime fromDate = Ut.date.parse(fromDateStr);
+        LocalDateTime toDate = Ut.date.parse(toDateStr);
+
+        return "fromDateStr : %s<br>toDateStr : %s".formatted(fromDateStr, toDateStr);
     }
 }
 
