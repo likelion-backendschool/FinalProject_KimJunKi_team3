451c06ab2d998ecd54654b97b12af98de773d5c3
unknown <wnsrl981228@gmail.com>
2022-11-07 10:53:50 +0900

    feat : 예외처리 로직 분리 + 아이디와 비밀번호 틀릴 경우 예외처리 구현

--- 4Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/api/common/exception/ExceptionType.java
+++ 4Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/api/common/exception/ExceptionType.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 @AllArgsConstructor
 public enum EventType {
+    // member 관련 exception
+    MEMBER_NOT_FOUND(NOT_FOUND, 100, "번째 회원을 찾을 수 없습니다.");
     UNHANDLED_EXCEPTION(BAD_REQUEST, 9999, "예상치 못한 예외입니다.");
+    
     private final HttpStatus httpStatus;
     private final int statusCode;
     private final String message;
